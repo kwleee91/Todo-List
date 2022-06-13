2931d0a4b27010da6e9a41166f685816e8288acf
KyungwonLee <kwleee91@naver.com>
2022-06-13 17:37:24 +0900

    Create Todo-list.tsx

---
+++ src/atoms.tsx
@@ -0,0 +1,32 @@
+import { atom, selector } from "recoil";
+import ToDo from "./components/ToDo";
+
+export interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
+export const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    if (category === "TO_DO")
+      return toDos.filter((toDo) => toDo.category === "TO_DO");
+    if (category === "DOING")
+      return toDos.filter((toDo) => toDo.category === "DOING");
+    if (category === "DONE")
+      return toDos.filter((toDo) => toDo.category === "DONE");
+  },
+});
